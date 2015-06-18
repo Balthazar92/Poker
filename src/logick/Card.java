4634bd1145e084d33ecc469a2565a605dbfc8bf0
balthazar92 <baltazar.0x100@gmail.com>
2015-06-18 23:15:19 +0500

    Class Card added

---
+++ src/logick/Card.java
@@ -0,0 +1,20 @@
+package logick;
+
+public class Card {
+	public static final int JACK = 11;
+	public static final int LADY = 12;
+	public static final int KING = 13;
+	public static final int ACE = 14;
+	public static final int SPADES = 0;
+	public static final int BAPTIZE = 1;
+	public static final int DIAMONDS = 2;
+	public static final int HEART = 3;
+	
+	public int value;
+	public int color;
+	
+	public Card(int value, int color){
+		this.value = value;
+		this.color = color;
+	}
+}
